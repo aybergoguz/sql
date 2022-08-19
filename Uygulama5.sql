bbff9eca3ac3a028b9154a7f493e32f4af902dea
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-08-20 00:28:48 +0300

    odev4  && DISTINCT COUNT

---
+++ Uygulama5.sql
@@ -0,0 +1,18 @@
+--SELECT DISTINCT rental_rate FROM film;
+--SELECT DISTINCT replacement_cost FROM film;
+--SELECT DISTINCT replacement_cost , rental_rate FROM film;
+--SELECT COUNT(*) FROM actor WHERE first_name ='Penelope';
+--SELECT COUNT(first_name) FROM actor WHERE first_name  LIKE 'A%';
+--SELECT COUNT(DISTINCT first_name) FROM actor;
+-- Odev4 --
+--1 SELECT DISTINCT replacement_cost from film;
+
+--2 SELECT COUNT(DISTINCT replacement_cost) from film;
+
+--3 SELECT COUNT(title) FROM film WHERE title LIKE 'T%' AND rating ='G';
+
+--4 SELECT COUNT(country) from country where country LIKE '_____';
+
+--5 SELECT COUNT(city) FROM city WHERE city ILIKE '%r';
+
+
