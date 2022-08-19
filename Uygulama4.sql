972f10887767d2d1a2b194c648e62a3c63b30436
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-08-19 22:43:32 +0300

    odev3 && LIKE ILIKE

---
+++ Uygulama4.sql
@@ -0,0 +1,19 @@
+-- SELECT * FROM customer WHERE first_name LIKE 'Ma%';
+-- SELECT * FROM customer WHERE first_name LIKE '%y';
+-- SELECT * FROM customer WHERE first_name LIKE 'M%y';
+-- SELECT * FROM customer WHERE first_name ILIKE 'a%y';
+-- SELECT * FROM customer WHERE first_name LIKE 'J_an';
+-- SELECT * FROM customer WHERE first_name LIKE 'J_';
+-- Odev3 --
+
+--1 SELECT country FROM country WHERE country LIKE 'A%a';
+
+--2 SELECT country FROM country WHERE country LIKE '______%n';
+
+--3 SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
+
+--4 SELECT * FROM film WHERE title LIKE 'C%' AND length >90 AND rental_rate=2.99;
+
+
+
+
