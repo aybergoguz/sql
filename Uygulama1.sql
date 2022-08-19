649cbe3a8e2109d8ad40bca397efcd294c351ad3
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-08-19 20:09:53 +0300

    sql select where

---
+++ Uygulama1.sql
@@ -0,0 +1,13 @@
+-- select colum1 from table;
+
+-- SELECT * from film;
+
+-- select first_name , last_name FROM actor;
+
+-- SELECT * FROM film WHERE replacement_cost = 12.99;
+
+-- SELECT * FROM actor WHERE first_name = 'Penelope' ;,
+
+-- SELECT * FROM actor WHERE first_name = 'Penelope' AND last_name = 'Monroe';
+
+-- SELECT * FROM film WHERE rental_rate=4.99 AND length > 90;
