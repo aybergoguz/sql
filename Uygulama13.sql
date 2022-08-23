a8982800101baca9e17ccc6700c79d51277a6e23
Ayberg Oguz <h.aybergoguz@gmail.com>
2022-08-23 21:44:10 +0300

    Odev10  &&   JOIN YAPILARI

---
+++ Uygulama13.sql
@@ -0,0 +1,7 @@
+-- odev10 --
+
+SELECT city , country FROM city LEFT JOIN country ON country.country_id = city.country_id;
+
+SELECT first_name , last_name , payment_id FROM customer RIGHT JOIN payment ON payment.customer_id = customer.customer_id;
+
+SELECT first_name , last_name , rental_id FROM customer FULL JOIN rental ON rental.customer_id = customer.customer_id;
